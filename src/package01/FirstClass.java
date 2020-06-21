7665114b88266549063eca89db594490fc093979
sekercih <hakkisekerci@gmail.com>
2020-06-21 21:48:07 +0300

    3. oluşum

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -3,7 +3,8 @@ package package01;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("ilk commit from repository");
 
 	}
 
